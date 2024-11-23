37750ba56cede87bf20c430212a7290035d36511
raycode22 <117015158+raycode22@users.noreply.github.com>
2024-11-23 16:42:29 +0800

    used decremeting for loop to pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
 };
+reverseString("hello");
+reverseString("hello, there");
+reverseString("123! abc! Hello, Odinite.");
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;

3baa1ed3fc0d2fb6887f53041fc7a445bfaee5af
raycode22 <117015158+raycode22@users.noreply.github.com>
2024-11-23 15:29:47 +0800

    enabled tests && passed all 7 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+    let result = "";
+    for (let i = 0; i < num; i++) {
+        result += str;
+    }
+    if (num < 0) {
+        return "ERROR";
+    }
+    return result;
 };
+
+repeatString("hey", 3);
+repeatString("hello", 10);
+repeatString("hi", 1);
+repeatString("bye", 0);
+repeatString("goodbye", -1);
+repeatString("odin", 100);
+repeatString("", 10);
 
 // Do not edit below this line
 module.exports = repeatString;

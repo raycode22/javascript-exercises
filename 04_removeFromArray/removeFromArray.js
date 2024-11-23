73a8db398a3e00f86e863689ada03e42b02f23e9
raycode22 <117015158+raycode22@users.noreply.github.com>
2024-11-23 22:51:08 +0800

    modified removeFromArray.js to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...itemsToRemove) {
+    return arr.filter((item) => !itemsToRemove.includes(item));
 };
-
+removeFromArray([1, 2, 3, 4], 2);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 2, 3], 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 // Do not edit below this line
 module.exports = removeFromArray;
